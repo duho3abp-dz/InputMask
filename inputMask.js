26396781d78f6be07c911e5fca29c015a569959b
duho3abp <DanZhidkof@yandex.ru>
2020-10-14 00:04:48 +0300

    made the main functionality, added the function of adding and checking the value

--- inputMask.js
+++ inputMask.js
@@ -2,14 +2,65 @@
 
 const inputMask = (inputClass) => {
     const input = document.querySelector(inputClass);
-    let value = '';
+    let value = '',
+        click = 0;
 
+    const testValue = () => {
+        if (value.length <= 3) {
+            input.value = `+7 (${value}`;
+        } else if (value.length > 3 && value.length <= 6) {
+            let first = value.slice(0, 3),
+                second = value.slice(3, 6);
 
+            input.value = `+7 (${first}) ${second}`;
+        } else if (value.length > 6 && value.length <= 8) {
+            let first = value.slice(0, 3),
+                second = value.slice(3, 6),
+                third = value.slice(6, 8);
+                
+            input.value = `+7 (${first}) ${second}-${third}`;
+        } else if (value.length > 8 && value.length <= 10) {
+            let first = value.slice(0, 3),
+                second = value.slice(3, 6),
+                third = value.slice(6, 8),
+                fourth = value.slice(8, 11);
+
+            input.value = `+7 (${first}) ${second}-${third}-${fourth}`;
+        }
+    };
+
+    const addValue = (num) => {
+        if (value.length < 10) {
+            value += num;
+            testValue();
+        } else {
+            testValue();
+        }
+        
+    };
+
+    input.addEventListener('click', e => {
+        click++;
+        if (click === 1) {
+            input.value = `+7 (`;
+        }
+    });
 
     input.addEventListener('input', e => {
-        if (+e.data) {
-            value += e.data;
+        console.dir(e);
+        if (+e.data || e.data === '0') {
+
+            addValue(e.data);
+
+        } else if (e.inputType === 'deleteContentBackward') {
+
+            value = value.slice(0, value.length - 1);
             input.value = `+7 (${value}`;
+
+        } else {
+
+            input.value = `+7 (${value}`;
+            
         }
     });
 };
