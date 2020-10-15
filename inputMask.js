abe092a086363dd8284f74685a46450c3b9199c9
duho3abp <DanZhidkof@yandex.ru>
2020-10-15 22:20:20 +0300

    functions for checking the number and rendering are written

--- inputMask.js
+++ inputMask.js
@@ -1,68 +1,68 @@
 'use strict';
 
-const inputMask = (inputClass) => {
-    const input = document.querySelector(inputClass);
-    let value = '',
-        click = 0;
+const inputMask = ({
+    inputIdentifier,
+    placeholder = '+7 (___) ___-__-__'
+}) => {
+    const input = document.querySelector(inputIdentifier);
+    let value = '';
 
-    const testValue = () => {
-        if (value.length <= 3) {
-            input.value = `+7 (${value}`;
-        } else if (value.length > 3 && value.length <= 6) {
-            let first = value.slice(0, 3),
-                second = value.slice(3, 6);
+    const checkForAvailability = (num) => num ? num : '_';
 
-            input.value = `+7 (${first}) ${second}`;
-        } else if (value.length > 6 && value.length <= 8) {
-            let first = value.slice(0, 3),
-                second = value.slice(3, 6),
-                third = value.slice(6, 8);
-                
-            input.value = `+7 (${first}) ${second}-${third}`;
-        } else if (value.length > 8 && value.length <= 10) {
-            let first = value.slice(0, 3),
-                second = value.slice(3, 6),
-                third = value.slice(6, 8),
-                fourth = value.slice(8, 11);
+    const valueProcessing = () => {
+        const numeral1 = checkForAvailability(value.slice(0, 1)),
+              numeral2 = checkForAvailability(value.slice(1, 2)),
+              numeral3 = checkForAvailability(value.slice(2, 3)),
+              numeral4 = checkForAvailability(value.slice(3, 4)),
+              numeral5 = checkForAvailability(value.slice(4, 5)),
+              numeral6 = checkForAvailability(value.slice(5, 6)),
+              numeral7 = checkForAvailability(value.slice(6, 7)),
+              numeral8 = checkForAvailability(value.slice(7, 8)),
+              numeral9 = checkForAvailability(value.slice(8, 9)),
+              numeral10 = checkForAvailability(value.slice(9, 10));
 
-            input.value = `+7 (${first}) ${second}-${third}-${fourth}`;
-        }
+        input.value = `+7 (${numeral1}${numeral2}${numeral3}) ${numeral4}${numeral5}${numeral6}-${numeral7}${numeral8}-${numeral9}${numeral10}`;
     };
 
-    const addValue = (num) => {
+    const addNumber = (num) => {
         if (value.length < 10) {
             value += num;
-            testValue();
+            valueProcessing();
         } else {
-            testValue();
+            valueProcessing();
         }
         
     };
 
-    input.addEventListener('click', e => {
-        click++;
-        if (click === 1) {
-            input.value = `+7 (`;
+    const removeValue = () => {
+        value = value.slice(0, value.length - 1);
+        valueProcessing();
+    };
+
+    input.placeholder = placeholder
+    input.style.caretColor = 'transparent';
+
+    input.addEventListener('blur', e => {
+        if (!value.length) {
+            input.value = ''
         }
     });
 
     input.addEventListener('input', e => {
-        console.dir(e);
         if (+e.data || e.data === '0') {
-
-            addValue(e.data);
-
+            addNumber(e.data);
         } else if (e.inputType === 'deleteContentBackward') {
-
-            value = value.slice(0, value.length - 1);
-            input.value = `+7 (${value}`;
-
+            removeValue();
         } else {
-
-            input.value = `+7 (${value}`;
-            
+            valueProcessing();
         }
     });
 };
 
-inputMask('.test-input');
+// *** If you want to test, uncomment the code below and comment out the export *** //
+
+export default inputMask;
+
+// inputMask({
+//     inputIdentifier: '.test-input'
+// });
